a97f7de23740f40275402fb0eb17a2d65a90589e
Zhao Xinjie <369520295@qq.com>
2013-02-25 22:28:59 +0800

    修改函数名称

--- AddLinkToHttpString.js
+++ AddLinkToHttpString.js
@@ -1,11 +1,11 @@
 
 //将字符串中的http地址转换为可点击的链接
 
-String.prototype.httpHtml = function(){
+String.prototype.addLinkToHttpString = function(){
         var reg = /(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)/g;
         return this.replace(reg, '<a href="$1$2">$1$2</a>');
 };
 
 var str = "将该字符串中的http地址转换为可点击的链接：https://github.com/zhaoxingjie/Js-functions";
-alert(str.httpHtml());
+alert(str.addLinkToHttpString());
 
